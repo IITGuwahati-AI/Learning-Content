58c85761b4234fc5d5fe6af8e7cf349f787d8a45
Devashish Taneja <32792385+dtanejaxyz@users.noreply.github.com>
2018-04-01 01:18:46 +0530

    Update devashish_170102026.py

--- devashish_170102026.py
+++ devashish_170102026.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.loadtxt('data.txt',delimiter='\t',skiprows=1)
+x = np.loadtxt('data.txt', delimiter = '\t', skiprows = 1 )
 
 #	DELIMITER HERE IS A TAB SPACE
 
 for i in range (1,999) :
-	if (x[i,0] == 1) :
-		plt.scatter(x[i,1],x[i,2],color='r',marker = 'x')
+	if ( x[i,0] == 1 ) :
+		plt.scatter(x[i,1], x[i,2], color = 'r', marker = 'x' )
 	else:
-		plt.scatter(x[i,1],x[i,2],color='b',marker = 'o')
+		plt.scatter(x[i,1], x[i,2], color = 'b', marker = 'o' )
 plt.title('features')
 plt.xlabel('1st attribute')
 plt.ylabel('2nd attribute')
@@ -21,14 +21,14 @@ for j in range (1,11):
 	for k in range (1,11) :
 		if (j < k) :
 			for i in range (1,999) :
-				if (x[i,0] == 1) :
-					plt.scatter(x[i,j],x[i,k],color='r',marker = 'x')
+				if ( x[i,0] == 1 ) :
+					plt.scatter(x[i,j], x[i,k], color = 'r' ,marker = 'x' )
 				else:
-					plt.scatter(x[i,j],x[i,k],color='b',marker = 'o')
+					plt.scatter(x[i,j], x[i,k], color = 'b' ,marker = 'o' )
 			plt.title('features')
 			plt.xlabel('1st attribute')
 			plt.ylabel('2nd attribute')
-			l=l+1
-			p=str(l)
+			l = l+1
+			p = str(l)
 			plt.savefig(p)
 			plt.clf()
