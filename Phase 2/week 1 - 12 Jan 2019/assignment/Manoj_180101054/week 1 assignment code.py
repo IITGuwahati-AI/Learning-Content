19b406d9ddcabf16cb5dff622aded23490771dbf
manojpaidimarri21 <44923214+manojpaidimarri21@users.noreply.github.com>
2019-05-22 16:17:15 +0530

    Update week 1 assignment code.py

--- Phase 2/week 1 - 12 Jan 2019/assignment/Manoj_180101054/week 1 assignment code.py
+++ Phase 2/week 1 - 12 Jan 2019/assignment/Manoj_180101054/week 1 assignment code.py
@@ -1,8 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 data = np.loadtxt('../data.txt',delimiter='\t',skiprows=1)
-for p in range(1,10):
-    for q in range(1,10):
+for p in range(1,11):
+    for q in range(1,11):
         if p<q:
           for i,j in enumerate(data[:,[0]]):
               if j==1:
