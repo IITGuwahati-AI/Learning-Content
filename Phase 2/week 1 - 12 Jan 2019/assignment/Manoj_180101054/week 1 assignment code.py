c0d70b3b3df3a8690d76199b9b88da964529ef09
manojpaidimarri21 <44923214+manojpaidimarri21@users.noreply.github.com>
2019-05-22 16:09:18 +0530

    Update week 1 assignment code.py

--- Phase 2/week 1 - 12 Jan 2019/assignment/Manoj_180101054/week 1 assignment code.py
+++ Phase 2/week 1 - 12 Jan 2019/assignment/Manoj_180101054/week 1 assignment code.py
@@ -1,12 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
-data = np.loadtxt('data.txt',delimiter='\t',skiprows=1)
-for i,j in enumerate(data[:,[0]]):
-   if j==1:
-       plt.scatter(data[i,9],data[i,8],s=5,color='r')
-   elif j==2:
-       plt.scatter(data[i,9],data[i,8],s=5,color='b')
-plt.title('F9 vs F8')
-plt.ylabel('Feature 8')
-plt.xlabel('Feature 9')
-plt.show()
+data = np.loadtxt('../data.txt',delimiter='\t',skiprows=1)
+for p in range(1,10):
+    for q in range(1,10):
+        if p<q:
+          for i,j in enumerate(data[:,[0]]):
+              if j==1:
+                  plt.scatter(data[i,p],data[i,q],s=5,color='r')
+              elif j==2:
+                  plt.scatter(data[i,p],data[i,q],s=5,color='b')
+          plt.title('F'+str(p)+'vs'+'F'+str(q))
+          plt.ylabel('feature'+str(q))
+          plt.xlabel('feature'+str(p))
+          plt.show()
+                  
+                 
