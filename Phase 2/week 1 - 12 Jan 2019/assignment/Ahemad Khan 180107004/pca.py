361eb854ca61a1804d93d8a74114a69f38c90f7a
ahemadkhan <46836407+ahemadkhan@users.noreply.github.com>
2019-05-22 23:26:53 +0530

    Update pca.py

--- Phase 2/week 1 - 12 Jan 2019/assignment/Ahemad Khan 180107004/pca.py
+++ Phase 2/week 1 - 12 Jan 2019/assignment/Ahemad Khan 180107004/pca.py
@@ -17,5 +17,8 @@ pca = PCA(n_components=10)
 
 pca.fit(X)
 
-print(pca.explained_variance_ratio_)
+var = pca.explained_variance_ratio_
+print(var)
+# prints the float type array which contains variance ratios for each principal component.
+# var = [0.23726612 0.17676103 0.15981101 0.09621538 0.09111189 0.08667718 0.07268585 0.04182227 0.01873639 0.01269897]
 
