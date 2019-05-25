c84d031f3f240f258a7e335707a830271087ed0a
Yash0330 <45749966+Yash0330@users.noreply.github.com>
2019-05-25 13:36:41 +0530

    Update Week_1_Assignment.py

--- Phase 2/week 1 - 12 Jan 2019/assignment/Yashwanth_180102081/Week_1_Assignment.py
+++ Phase 2/week 1 - 12 Jan 2019/assignment/Yashwanth_180102081/Week_1_Assignment.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 d = np.loadtxt('data.txt', skiprows=1)
-for i,label in enumerate(d[:,[0]]) :
-    if label==1 :
-        plt.scatter(d[i,[1]], d[i,[2]], s=1, color='red')
-    elif label==2 :
-        plt.scatter(d[i,[1]], d[i,[2]], s=1, color='blue')
-
-plt.xlabel('Feature-1')
-plt.ylabel('Feature-2')
-plt.title('F1 vs F2')
-plt.show()
+for i in range(1,11):
+    for j in range(1,11):
+        if i<j:
+          for k,lablel in enumerate(d[:,[0]]):
+              if lablel==1:
+                  plt.scatter(d[k,i],d[k,j],s=1,color='r')
+              elif lablel==2:
+                  plt.scatter(d[k,i],d[k,j],s=1,color='b')
+          plt.title('F'+str(i)+'vs'+'F'+str(j))
+          plt.ylabel('Feature'+str(j))
+          plt.xlabel('Feature'+str(i))
+          plt.show()
