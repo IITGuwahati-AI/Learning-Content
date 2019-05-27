de27fcf268780562dbc69913382de43962551bb2
abhishekk2 <43750198+abhishekk2@users.noreply.github.com>
2019-05-27 12:24:19 +0530

    Week 1 assignment (without bonus)

---
+++ Phase 2/week 1 - 12 Jan 2019/assignment/week1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = np.loadtxt(fname = "/home/abhishek/data.txt", skiprows=1)
+
+for i in range(1,10):
+    for j in range(i+1,11):
+       for k in range(0,len(dataset)-1):
+          if dataset[k,0]== 1:
+            plt.scatter(dataset[k,i], dataset[k, j], s=3, color= 'red')
+          elif dataset[k,0] == 2:
+            plt.scatter(dataset[k,i], dataset[k, j], s=3, color= 'blue')
+          
+       plt.ylabel('Feature-' + str(j))
+       plt.xlabel('Feature-' + str(i))
+       plt.title('F' + str(i) + ' vs F' + str(j))
+       plt.savefig('feature'+str(i)+'vs'+str(j)+'.png')
+       plt.show()
+
+##Feature-1 and Feature-2 classify two labels perfectly.
+      
+
+
+
+
+
+
+
