d2d966b2212c24c59d57505f5bc2ad28ae94f1ff
krishna-kaushik <46679308+krishna-kaushik@users.noreply.github.com>
2019-12-05 23:28:46 +0530

    update pca code

--- Phase 3/week 1/pcaassignment.py
+++ Phase 3/week 1/pcaassignment.py
@@ -3,17 +3,18 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-data_file = np.loadtxt('C:/Users/Administrator/Desktop/data.txt',delimiter='\t',skiprows=1)
+data_doc = np.loadtxt('C:/Users/Administrator/Desktop/data.txt',delimiter='\t',skiprows=1)
 for i in range(1,11):
     for j in range(i+1,11):
-        x=np.c_[data_file[:,i],data_file[:,j]]
+        x=np.c_[data_doc[:,i],data_doc[:,j]]
         x = StandardScaler().fit_transform(x)
         pca=PCA(n_components=1)
         p = pca.fit_transform(x)
-        l1=plt.plot(p[data_file[:,0]==1,0],'m.',label='label1')
-        l2=plt.plot(p[data_file[:,0]==2,0],'y.',label='label2')
-        plt.xlabel('feature'+str(i))
+        l1=plt.plot(p[data_doc[:,0]==1,0],'m.',label='label1')
+        l2=plt.plot(p[data_doc[:,0]==2,0],'y.',label='label2')
+       
         plt.ylabel('feature'+str(j))
+        plt.xlabel('feature'+str(i))
         plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,ncol=2, mode="expand", borderaxespad=0.)
         plt.title('PCA:feature'+str(i)+'vs'+str(j),fontweight="bold")
         plt.savefig('PCA:f'+str(i)+'VS'+'f'+str(j)+'.png')
