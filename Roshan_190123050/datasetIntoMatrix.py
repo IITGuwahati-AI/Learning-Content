b27d2ed74a302922cf2db3cc8accb63ce1e7775e
Roshan Kumar <56060325+Roshan-Mehta@users.noreply.github.com>
2020-03-31 12:26:34 +0530

    Add files via upload

---
+++ Roshan_190123050/datasetIntoMatrix.py
@@ -0,0 +1,4 @@
+import numpy as np 
+label,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10=np.loadtxt('data.txt',skiprows=1,unpack=True)
+matrixOfData=np.asmatrix([label,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10])
+print(matrixOfData.T)
