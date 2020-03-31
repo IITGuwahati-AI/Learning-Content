8de4dd51d9a639e3e7b4173383362a0d57f0384d
Roshan Kumar <56060325+Roshan-Mehta@users.noreply.github.com>
2020-03-31 12:29:41 +0530

    Update datasetIntoMatrix.py

--- Roshan_190123050/datasetIntoMatrix.py
+++ Roshan_190123050/datasetIntoMatrix.py
@@ -1,4 +1,6 @@
 import numpy as np 
 label,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10=np.loadtxt('data.txt',skiprows=1,unpack=True)
 matrixOfData=np.asmatrix([label,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10])
-print(matrixOfData.T)
+print(matrixOfData)
+
+#code by Roshan_190123050
